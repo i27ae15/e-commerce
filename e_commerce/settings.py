3434712fcd15886796391120fea866f1374e1521
Andres Melendes <andresruse18@gmail.com>
2022-02-08 23:46:05 -0400

    Complete

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['*']
-# ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 # Application definition
 
