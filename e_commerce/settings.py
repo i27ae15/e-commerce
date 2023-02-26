856ee024c2a4e08b899a3ef089d98c59f50228f4
i27ae15 <58411306+i27ae15@users.noreply.github.com>
2023-02-26 12:53:03 -0400

    add allowed hosts

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', 'web-production-f829.up.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', 'web-production-86b7.up.railway.app']
 
 # Application definition
 
